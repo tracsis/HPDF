b3b49dfce3b8aef0065ae921d2a8d0078afbd018
David Hewson <david.hewson@tracsis.com>
2018-08-14 16:29:25 +0100

    embed fonts
    also some whitespace changes got in :(

--- Graphics/PDF/Fonts/AFMParser.hs
+++ Graphics/PDF/Fonts/AFMParser.hs
@@ -1,7 +1,7 @@
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE ExistentialQuantification  #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell            #-}
 ---------------------------------------------------------
 -- |
 -- Copyright   : (c) 2006-2016, alpheccar.org
@@ -20,48 +20,52 @@ module Graphics.PDF.Fonts.AFMParser(
     , Metric(..)
     , KX(..)
     , parseFont
-    ) where 
+    ) where
 
-import Text.ParserCombinators.Parsec hiding(space)
-import Text.Parsec(modifyState,getState)
-import Text.Parsec.Prim(parserZero)
-import Data.Char(toUpper)
-import qualified Data.Map.Strict as M
-import Graphics.PDF.Fonts.Font(emptyFontStructure)
-import Paths_HPDF
-import Graphics.PDF.LowLevel.Types
-import Graphics.PDF.Fonts.Encoding(PostscriptName)
-import Graphics.PDF.Fonts.FontTypes
-import Control.Monad
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BChar8
+import           Control.Monad
+import           Data.Char                     (toUpper)
+import           Data.FileEmbed
+import qualified Data.Map.Strict               as M
+import           Data.Maybe                    (fromJust)
+import           Graphics.PDF.Fonts.Encoding   (PostscriptName)
+import           Graphics.PDF.Fonts.Font       (emptyFontStructure)
+import           Graphics.PDF.Fonts.FontTypes
+import           Graphics.PDF.LowLevel.Types
+import           Paths_HPDF
+import           Text.Parsec                   (getState, modifyState)
+import           Text.Parsec.Prim              (parserZero)
+import           Text.ParserCombinators.Parsec hiding (space)
 
-data Metric = Metric { charCode :: Int
+data Metric = Metric { charCode    :: Int
                      , metricWidth :: Int
-                     , name :: String
-                     , bounds :: [Double]
+                     , name        :: String
+                     , bounds      :: [Double]
                      }
                      deriving(Eq,Show)
-                     
-data EncodingScheme = AFMAdobeStandardEncoding 
+
+data EncodingScheme = AFMAdobeStandardEncoding
                     | AFMFontSpecific
                     | AFMUnsupportedEncoding
                     deriving(Eq,Read,Show)
 
-data KX = KX String String Int  
-        deriving(Eq,Ord,Show)  
+data KX = KX String String Int
+        deriving(Eq,Ord,Show)
 
-data AFMFont = AFMFont { metrics :: [Metric]
-                       , underlinePosition :: Int
+data AFMFont = AFMFont { metrics            :: [Metric]
+                       , underlinePosition  :: Int
                        , underlineThickness :: Int
-                       , afmAscent :: Int
-                       , afmDescent :: Int
-                       , kernData :: Maybe [KX]
-                       , type1BaseFont :: String
-                       , encodingScheme :: EncodingScheme
-                       , afmItalic :: Double 
-                       , afmCapHeight :: Int
-                       , afmBBox :: [Double]
-                       , afmFixedPitch :: Bool
-                       , afmSymbolic :: Bool
+                       , afmAscent          :: Int
+                       , afmDescent         :: Int
+                       , kernData           :: Maybe [KX]
+                       , type1BaseFont      :: String
+                       , encodingScheme     :: EncodingScheme
+                       , afmItalic          :: Double
+                       , afmCapHeight       :: Int
+                       , afmBBox            :: [Double]
+                       , afmFixedPitch      :: Bool
+                       , afmSymbolic        :: Bool
                        }
                        deriving(Eq,Show)
 
@@ -83,9 +87,9 @@ emptyAFM = AFMFont { metrics = []
                    , afmFixedPitch = False
                    , afmSymbolic = False
                    }
-                    
+
 capitalize :: String -> String
-capitalize [] = []
+capitalize []    = []
 capitalize (h:t) = toUpper h : t
 
 
@@ -96,77 +100,77 @@ toEndOfLine :: AFMParser ()
 toEndOfLine = do _ <- many (noneOf "\r\n")
                  line
                  return ()
-                 
+
 getString :: AFMParser String
-getString = do 
+getString = do
   c <- many1 (alphaNum <|> oneOf "-+")
   line
   return c
-               
+
 getInt :: AFMParser Int
 getInt  = do c <- getString
              return $ read c
-              
+
 getFloat :: AFMParser Double
-getFloat = do 
+getFloat = do
                 c <- many1 (alphaNum <|> oneOf ".-+")
                 line
                 return $ read c
-              
+
 getBool :: AFMParser Bool
 getBool = do c <- getString
              return $ read (capitalize c)
-               
+
 data CharacterSet = ExtendedRoman
                   | Special
                   deriving(Eq,Read,Show)
-    
+
 data Weight = Medium
             | Bold
             | Roman
             deriving(Eq,Read,Show)
-                 
-  
-array :: AFMParser [String]  
+
+
+array :: AFMParser [String]
 array = sepEndBy (many1 (oneOf "-+0123456789")) (many1 (oneOf " "))
-                 
+
 getArray :: AFMParser [Double]
 getArray  = do c <- array
                line
                return . map read $ c
-                 
 
-           
+
+
 getEncoding :: AFMParser EncodingScheme
-getEncoding = do 
+getEncoding = do
   c <- getString
-  case c of 
+  case c of
     "AdobeStandardEncoding" -> return AFMAdobeStandardEncoding
-    "FontSpecific" -> return AFMFontSpecific 
-    _ -> return  AFMUnsupportedEncoding     
-                                           
+    "FontSpecific"          -> return AFMFontSpecific
+    _                       -> return  AFMUnsupportedEncoding
+
 number :: AFMParser Int
 number  = do c <- many1 (oneOf "-+0123456789")
              return $ read c
-         
+
 data Elem = C Int
           | WX Int
           | N String
           | B [Double]
           | L
-          deriving(Eq,Read,Show)    
-               
+          deriving(Eq,Read,Show)
+
 metricElem :: AFMParser Elem
 metricElem  = do _ <- char 'C'
                  spaces
-                 c <- number 
+                 c <- number
                  return $ C c
               <|>
               do _ <- string "WX"
                  spaces
-                 c <- number   
+                 c <- number
                  return $ WX c
-              <|> 
+              <|>
               do _ <- char 'N'
                  spaces
                  c <- many1 (alphaNum <|> char '.')
@@ -175,32 +179,32 @@ metricElem  = do _ <- char 'C'
               do _ <- char 'B'
                  spaces
                  c <- array
-                 return . B . map read $ c   
-              <|> 
+                 return . B . map read $ c
+              <|>
               do _ <- char 'L'
                  spaces
                  _ <- many1 letter
                  spaces
                  _ <- many1 letter
                  return L
-                        
+
 mkMetric :: [Elem] -> Metric
-mkMetric l = foldr addElem (Metric (-1) 0 "" []) l     
+mkMetric l = foldr addElem (Metric (-1) 0 "" []) l
  where
-     addElem  (C c) m = m {charCode=c}
+     addElem  (C c) m  = m {charCode=c}
      addElem  (WX c) m = m {metricWidth=c}
-     addElem  (N s) m = m {name=s}
-     addElem  (B b) m = m {bounds=b}
-     addElem  _ m = m         
-                          
+     addElem  (N s) m  = m {name=s}
+     addElem  (B b) m  = m {bounds=b}
+     addElem  _ m      = m
+
 charMetric :: AFMParser Metric
 charMetric = do
-       l <- sepEndBy metricElem (many1 (oneOf "; ")) 
-       line 
+       l <- sepEndBy metricElem (many1 (oneOf "; "))
+       line
        return . mkMetric $ l
-       
 
-       
+
+
 kernPair :: AFMParser KX
 kernPair = do _ <- string "KPX"
               spaces
@@ -211,49 +215,49 @@ kernPair = do _ <- string "KPX"
               nb <- many1 (oneOf "-+0123456789")
               line
               return $ KX namea nameb (read nb)
-                       
 
-              
-keyword :: String -> AFMParser () -> AFMParser () 
-keyword s action = do 
+
+
+keyword :: String -> AFMParser () -> AFMParser ()
+keyword s action = do
   _ <- string s
   spaces
   action
   return ()
 
-header :: String -> AFMParser () 
-header s = do 
-  _ <- string s  
-  toEndOfLine 
+header :: String -> AFMParser ()
+header s = do
+  _ <- string s
+  toEndOfLine
   return ()
 
-notHeader :: String -> AFMParser () 
-notHeader s = do 
+notHeader :: String -> AFMParser ()
+notHeader s = do
   r <- many1 alphaNum
-  if s == r 
-    then 
-      parserZero 
-    else do 
+  if s == r
+    then
+      parserZero
+    else do
       toEndOfLine
 
-specific :: AFMParser () 
-specific = choice [ try $ keyword "FontName" (getString >>= \val -> modifyState $ \st -> st {type1BaseFont = val}) 
-                  , try $ keyword "UnderlinePosition" (getInt >>= \val -> modifyState $ \st -> st {underlinePosition = val}) 
+specific :: AFMParser ()
+specific = choice [ try $ keyword "FontName" (getString >>= \val -> modifyState $ \st -> st {type1BaseFont = val})
+                  , try $ keyword "UnderlinePosition" (getInt >>= \val -> modifyState $ \st -> st {underlinePosition = val})
                   , try $ keyword "UnderlineThickness" (getInt >>= \val -> modifyState $ \st -> st {underlineThickness = val})
                   , try $ keyword "EncodingScheme" (getEncoding >>= \val -> modifyState $ \st -> st {encodingScheme = val})
-                  , try $ keyword "CapHeight" (getInt >>= \val -> modifyState $ \st -> st {afmCapHeight = val}) 
+                  , try $ keyword "CapHeight" (getInt >>= \val -> modifyState $ \st -> st {afmCapHeight = val})
                   , try $ keyword "Ascender" (getInt >>= \val -> modifyState $ \st -> st {afmAscent = val})
-                  , try $ keyword "Descender" (getInt >>= \val -> modifyState $ \st -> st {afmDescent = val}) 
-                  , try $ keyword "ItalicAngle" (getFloat >>= \val -> modifyState $ \st -> st {afmItalic = val}) 
-                  , try $ keyword "IsFixedPitch" (getBool >>= \val -> modifyState $ \st -> st {afmFixedPitch = val}) 
-                  , try $ keyword "FontBBox" (getArray >>= \val -> modifyState $ \st -> st {afmBBox = val}) 
+                  , try $ keyword "Descender" (getInt >>= \val -> modifyState $ \st -> st {afmDescent = val})
+                  , try $ keyword "ItalicAngle" (getFloat >>= \val -> modifyState $ \st -> st {afmItalic = val})
+                  , try $ keyword "IsFixedPitch" (getBool >>= \val -> modifyState $ \st -> st {afmFixedPitch = val})
+                  , try $ keyword "FontBBox" (getArray >>= \val -> modifyState $ \st -> st {afmBBox = val})
                   , try $ notHeader "StartCharMetrics"
                   ]
 
 getKernData :: AFMParser (Maybe [KX])
-getKernData = do 
+getKernData = do
             { header "StartKernData"
-            ; header "StartKernPairs" 
+            ; header "StartKernPairs"
             ; k <- many1 kernPair
             ; header "EndKernPairs"
             ; header "EndKernData"
@@ -261,54 +265,54 @@ getKernData = do
             }
 
 afm :: AFMParser AFMFont
-afm = 
-  do  
+afm =
+  do
     header "StartFontMetrics"
-    _ <- many1 specific 
+    _ <- many1 specific
     header "StartCharMetrics"
     charMetrics <- many1 charMetric
     header "EndCharMetrics"
     kerns <- option Nothing getKernData
     _ <- string "EndFontMetrics"
-    
-    modifyState $ \a -> a { metrics = charMetrics 
+
+    modifyState $ \a -> a { metrics = charMetrics
                           , kernData = kerns
                           }
 
-    a <- getState 
+    a <- getState
     let [_,ymin,_,ymax] = afmBBox a
-    if afmAscent a == 0 
+    if afmAscent a == 0
     then
-       if afmCapHeight a /= 0 
+       if afmCapHeight a /= 0
           then
               return $ a { afmAscent = afmCapHeight a
                          }
           else
               let h = floor (ymax - ymin) in
-              return $ a { afmAscent = h 
-                         , afmDescent = 0 
+              return $ a { afmAscent = h
+                         , afmDescent = 0
                          }
     else
        return $ a
 
-addMetric :: M.Map PostscriptName GlyphCode -> Metric -> FontStructure -> FontStructure 
-addMetric nameToGlyph m fs = 
+addMetric :: M.Map PostscriptName GlyphCode -> Metric -> FontStructure -> FontStructure
+addMetric nameToGlyph m fs =
     let c = M.lookup (name m) nameToGlyph
-        fs' = case c of 
-                Just glyphCode -> 
+        fs' = case c of
+                Just glyphCode ->
                   fs { widthData = M.insert (fromIntegral glyphCode) (fromIntegral $ metricWidth m) (widthData fs)}
                 Nothing -> fs
-    in 
-    case (name m) of 
-      "space" -> fs' {space = fromIntegral $ charCode m}
+    in
+    case (name m) of
+      "space"  -> fs' {space = fromIntegral $ charCode m}
       "hyphen" -> fs' {hyphen = Just (fromIntegral $ charCode m)}
-      _ -> fs'
+      _        -> fs'
 
-addKern :: M.Map String GlyphCode -> KX -> FontStructure -> FontStructure 
-addKern d (KX sa sb c) fs = 
-  let caM = M.lookup sa d 
+addKern :: M.Map String GlyphCode -> KX -> FontStructure -> FontStructure
+addKern d (KX sa sb c) fs =
+  let caM = M.lookup sa d
       cbM = M.lookup sb d
-  in 
+  in
   case (caM,cbM) of
     (Just ca, Just cb) -> fs {kernMetrics = M.insert (GlyphPair ca cb) (fromIntegral c) (kernMetrics fs)}
     _ -> fs
@@ -317,14 +321,14 @@ addKern d (KX sa sb c) fs =
 -- the postscript names.
 -- Otherwise we use the encoding we found in the afm file.
 -- It is used to force MacRomanEncoding on not symbolic default fonts.
-fontToStructure :: AFMFont 
-                -> M.Map PostscriptName Char 
+fontToStructure :: AFMFont
+                -> M.Map PostscriptName Char
                 -> Maybe (M.Map PostscriptName GlyphCode)
-                -> FontStructure 
+                -> FontStructure
 fontToStructure a enc maybeMapNameToGlyph =
-  let h = (afmAscent a - afmDescent a) 
+  let h = (afmAscent a - afmDescent a)
       fs = emptyFontStructure { descent = fromIntegral $ - (afmDescent a)
-                              , height = fromIntegral $ h 
+                              , height = fromIntegral $ h
                               , ascent = fromIntegral $ afmAscent a
                               , fontBBox = afmBBox a
                               , italicAngle = afmItalic a
@@ -341,48 +345,54 @@ fontToStructure a enc maybeMapNameToGlyph =
                               , baseFont = type1BaseFont a
                               }
       addName m d | charCode m == -1 = d
-                  | otherwise = M.insert (name m) (fromIntegral $ charCode m) d 
+                  | otherwise = M.insert (name m) (fromIntegral $ charCode m) d
       nameToGlyph = maybe (foldr addName M.empty (metrics a)) id maybeMapNameToGlyph
       fs1 = foldr (addMetric nameToGlyph) fs (metrics a)
-      addEncodingMapping (pname,glyphcode) d = 
-         let unicodeM = M.lookup pname enc 
-         in 
-         case unicodeM of 
-          Nothing -> d 
-          Just code -> M.insert code glyphcode d 
+      addEncodingMapping (pname,glyphcode) d =
+         let unicodeM = M.lookup pname enc
+         in
+         case unicodeM of
+          Nothing   -> d
+          Just code -> M.insert code glyphcode d
       mapping = foldr addEncodingMapping M.empty (M.toList nameToGlyph)
       fs2 = fs1 { encoding = mapping}
   in
   case kernData a of
     Nothing -> fs2
-    Just k -> foldr (addKern nameToGlyph) fs2 k
+    Just k  -> foldr (addKern nameToGlyph) fs2 k
 
 afmParseFromFile :: AFMParser AFMFont -> FilePath -> IO (Either ParseError AFMFont)
-afmParseFromFile p path = do 
-  l <- readFile path 
+afmParseFromFile p path = do
+  l <- readFile path
   return $ runParser p emptyAFM path l
 
+afmParseFromFileBytes :: AFMParser AFMFont -> FilePath -> B.ByteString -> IO (Either ParseError AFMFont)
+afmParseFromFileBytes p path bytes = return $ runParser p emptyAFM path (BChar8.unpack bytes)
+
+core14_AFMsDir :: M.Map FilePath B.ByteString
+core14_AFMsDir = M.fromList $(embedDir "Core14_AFMs")
+
 parseFont :: Either String String -> IO (Maybe AFMFont)
-parseFont (Left s) = do
-    path <- getDataFileName s
-    r <- afmParseFromFile afm path
+parseFont (Left core14_file) = do
+    let bytes = fromJust $ M.lookup core14_file core14_AFMsDir
+    r <- afmParseFromFileBytes afm core14_file bytes
     case r of
-      Left e -> error (show e)
+      Left e  -> error (show e)
       Right f -> return $ Just f
 parseFont (Right path) = do
     r <- afmParseFromFile afm path
     case r of
-      Left e -> error (show e)
+      Left e  -> error (show e)
       Right f -> return $ Just f
 
 getFont :: Either String AFMFont
         -> M.Map PostscriptName Char  -- ^ Glyph name to unicode
         -> Maybe (M.Map PostscriptName GlyphCode)  -- ^ Glyph name to glyph code if not standard coding
         -> IO (Maybe FontStructure)
-getFont (Left s) enc nameToGlyph = do 
-  result <- parseFont (Left s) 
-  case result of 
-    Nothing -> return Nothing 
-    Just r -> return (Just $ fontToStructure r enc nameToGlyph)
+getFont (Left s) enc nameToGlyph = do
+  result <- parseFont (Left s)
+  case result of
+    Nothing -> return Nothing
+    Just r  -> return (Just $ fontToStructure r enc nameToGlyph)
 getFont (Right result) enc nameToGlyph = return . Just $ fontToStructure result enc nameToGlyph
 
--- Graphics/PDF/Fonts/Encoding.hs
+++ Graphics/PDF/Fonts/Encoding.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification  #-}
+{-# LANGUAGE TemplateHaskell #-}
 ---------------------------------------------------------
 -- |
 -- Copyright   : (c) 2006-2016, alpheccar.org
@@ -17,36 +16,41 @@ module Graphics.PDF.Fonts.Encoding(
     , Encodings(..)
     , PostscriptName
     , parseMacEncoding
-    ) where 
+    ) where
 
-import Graphics.PDF.LowLevel.Types
-import qualified Data.Map.Strict as M
-import System.FilePath 
-import Paths_HPDF
-import qualified Data.ByteString.Char8 as C 
-import Data.Char(digitToInt)
-import Data.Maybe(mapMaybe)
+import qualified Data.ByteString
+import qualified Data.ByteString.Char8       as C
+import           Data.Char                   (digitToInt)
+import           Data.FileEmbed
+import qualified Data.Map.Strict             as M
+import           Data.Maybe                  (mapMaybe, fromJust)
+import           Graphics.PDF.LowLevel.Types
+import           Paths_HPDF
+import           System.FilePath
 
-type PostscriptName = String 
+type PostscriptName = String
 
-data Encodings = AdobeStandardEncoding 
+data Encodings = AdobeStandardEncoding
                | ZapfDingbatsEncoding
                deriving(Eq)
 
-isLine :: C.ByteString -> Bool 
+encodingsDir :: M.Map FilePath Data.ByteString.ByteString
+encodingsDir = M.fromList $(embedDir "Encodings")
+
+isLine :: C.ByteString -> Bool
 isLine c | not (C.null c) = C.head c /= '#'
          | otherwise = False
 
-from4Hexa :: C.ByteString -> Int 
-from4Hexa a = sum . map (\(x,y) -> x * y) $ zip (map digitToInt . C.unpack $ a)  (map (\x -> 16^x) ([3,2,1,0] :: [Int]))
+from4Hexa :: C.ByteString -> Int
+from4Hexa a = sum . map (uncurry (*)) $ zip (map digitToInt . C.unpack $ a)  (map (\x -> 16^x) ([3,2,1,0] :: [Int]))
 
-from3Octal:: C.ByteString -> Int 
-from3Octal a = sum . map (\(x,y) -> x * y) $ zip (map digitToInt . C.unpack $ a)  (map (\x -> 8^x) ([2,1,0] :: [Int]))
+from3Octal:: C.ByteString -> Int
+from3Octal a = sum . map (uncurry (*)) $ zip (map digitToInt . C.unpack $ a)  (map (\x -> 8^x) ([2,1,0] :: [Int]))
 
 
 toData :: [C.ByteString] -> Maybe (PostscriptName,Char)
 toData (a:b:_) = Just (C.unpack a,toEnum . from4Hexa $ b)
-toData _ = Nothing
+toData _       = Nothing
 
 toMacData :: [C.ByteString] -> Maybe (PostscriptName,GlyphCode)
 toMacData (name:_:mac:_) | C.unpack mac == "-" = Nothing
@@ -54,18 +58,16 @@ toMacData (name:_:mac:_) | C.unpack mac == "-" = Nothing
 toMacData _ = Nothing
 
 parseGlyphListEncoding :: String -> IO (M.Map PostscriptName Char)
-parseGlyphListEncoding name = do 
-  path <- getDataFileName name 
-  l <- C.readFile path 
-  return (M.fromList . mapMaybe (toData  . C.split ';') . filter isLine . C.lines $ l)
+parseGlyphListEncoding name = do
+  let bytes = fromJust $ M.lookup name encodingsDir
+  return (M.fromList . mapMaybe (toData  . C.split ';') . filter isLine . C.lines $ bytes)
 
 parseMacEncoding :: IO (M.Map PostscriptName GlyphCode)
-parseMacEncoding = do 
-    path <- getDataFileName "Encodings/pdfencodings.txt" 
-    l <- C.readFile path 
-    return . M.fromList . mapMaybe (toMacData . C.split '\t') . tail . C.lines $ l
+parseMacEncoding = do
+  let bytes = fromJust $ M.lookup "pdfencodings.txt" encodingsDir
+  return . M.fromList . mapMaybe (toMacData . C.split '\t') . tail . C.lines $ bytes
 
 
 getEncoding :: Encodings -> IO (M.Map PostscriptName Char)
-getEncoding AdobeStandardEncoding = parseGlyphListEncoding $ "Encodings" </> "glyphlist" <.> "txt"
-getEncoding ZapfDingbatsEncoding= parseGlyphListEncoding $ "Encodings" </> "zapfdingbats" <.> "txt"
+getEncoding AdobeStandardEncoding = parseGlyphListEncoding $ "glyphlist" <.> "txt"
+getEncoding ZapfDingbatsEncoding= parseGlyphListEncoding $ "zapfdingbats" <.> "txt"
